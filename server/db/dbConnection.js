9ff8411b66209fa51a4aee91ad5aafeb6084d687
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2021-02-09 17:09:32 -0500

    Kepp it this way for master branch

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,10 +1,18 @@
 const {Client} = require('pg');
+const CONFIG = require('./config');
+
+const DB_URL = process.env.DB_URL || CONFIG.DB_URL;
+const DB_PORT = process.env.DB_PORT || CONFIG.DB_PORT;
+const DB_USER = process.env.DB_USER || CONFIG.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD || CONFIG.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME || CONFIG.DB_NAME;
 
 const dbClient = new Client({
-    connectionString: process.env.DB_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    host: DB_URL,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 });
 
 module.exports = dbClient
